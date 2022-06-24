b347f53c58b6159d21abe285cfd89e13826e5f5c
Austine Ezemelue <draustine@yahoo.com>
2022-06-24 19:49:22 +0100

    Revision

---
+++ demo/src/main/java/com/example/demo/student/StudentConfig.java
@@ -0,0 +1,46 @@
+package com.example.demo.student;
+
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import java.time.LocalDate;
+import java.util.List;
+
+@Configuration
+public class StudentConfig {
+
+    @Bean
+    CommandLineRunner commandLineRunner(StudentRepository repository) {
+        return args -> {
+
+            Student austine = new Student(
+                    "Austine Ezemelue",
+                    "draustine@yahoo.com",
+                    LocalDate.of(1973, 2, 12)
+            );
+
+            Student john = new Student(
+                    "John Ezemelue",
+                    "john@yahoo.com",
+                    LocalDate.of(1975, 5, 21)
+            );
+
+            Student ronke = new Student(
+                    "Ronke Adebayo",
+                    "ronke@yahoo.com",
+                    LocalDate.of(2000, 12, 5)
+            );
+
+            Student bayo = new Student(
+                    "Bayo Adedokun",
+                    "bayo@yahoo.com",
+                    LocalDate.of(1970, 11, 15)
+            );
+
+            repository.saveAll(
+                    List.of(austine, john, bayo)
+            );
+        };
+    }
+}
